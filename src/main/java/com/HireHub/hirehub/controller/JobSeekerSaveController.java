769e0db833e39bf5fbdbb1c0203b3f6896e5cb52
Saniya-Bagwan630 <saniyabagwan630@gmail.com>
2026-07-30 01:14:55 +0530

    implement download resume feature

---
+++ src/main/java/com/HireHub/hirehub/controller/JobSeekerSaveController.java
@@ -0,0 +1,74 @@
+package com.HireHub.hirehub.controller;
+
+import com.HireHub.hirehub.entity.JobPostActivity;
+import com.HireHub.hirehub.entity.JobSeekerProfile;
+import com.HireHub.hirehub.entity.JobSeekerSave;
+import com.HireHub.hirehub.entity.Users;
+import com.HireHub.hirehub.services.JobPostActivityService;
+import com.HireHub.hirehub.services.JobSeekerProfileService;
+import com.HireHub.hirehub.services.JobSeekerSaveService;
+import com.HireHub.hirehub.services.UsersService;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.*;
+
+@Controller
+public class JobSeekerSaveController {
+
+    private final UsersService usersService;
+    private final JobSeekerProfileService jobSeekerProfileService;
+    private final JobPostActivityService jobPostActivityService;
+    private final JobSeekerSaveService jobSeekerSaveService;
+
+    public JobSeekerSaveController(UsersService usersService, JobSeekerProfileService jobSeekerProfileService, JobPostActivityService jobPostActivityService, JobSeekerSaveService jobSeekerSaveService) {
+        this.usersService = usersService;
+        this.jobSeekerProfileService = jobSeekerProfileService;
+        this.jobPostActivityService = jobPostActivityService;
+        this.jobSeekerSaveService = jobSeekerSaveService;
+    }
+
+    @PostMapping("job-details/save/{id}")
+    public String save(@PathVariable("id") int id, JobSeekerSave jobSeekerSave) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users user = usersService.findByEmail(currentUsername);
+            Optional<JobSeekerProfile> seekerProfile = jobSeekerProfileService.getOne(user.getUserId());
+            JobPostActivity jobPostActivity = jobPostActivityService.getOne(id);
+            if (seekerProfile.isPresent() && jobPostActivity != null) {
+                jobSeekerSave.setJob(jobPostActivity);
+                jobSeekerSave.setUserId(seekerProfile.get());
+            } else {
+                throw new RuntimeException("User not found");
+            }
+            jobSeekerSaveService.addNew(jobSeekerSave);
+        }
+        return "redirect:/dashboard/";
+    }
+
+    @GetMapping("saved-jobs/")
+    public String savedJobs(Model model) {
+
+        List<JobPostActivity> jobPost = new ArrayList<>();
+        Object currentUserProfile = usersService.getCurrentUserProfile();
+
+        List<JobSeekerSave> jobSeekerSaveList = jobSeekerSaveService.getCandidatesJob((JobSeekerProfile) currentUserProfile);
+        for (JobSeekerSave jobSeekerSave : jobSeekerSaveList) {
+            jobPost.add(jobSeekerSave.getJob());
+        }
+
+        model.addAttribute("jobPost", jobPost);
+        model.addAttribute("user", currentUserProfile);
+
+        return "saved-jobs";
+    }
+}
